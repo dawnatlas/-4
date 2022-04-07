de0f0b48a5d267dbbf06e0d8559e4c1446e1f6ff
Анастасия Аникина <102908197+dawnatlas@users.noreply.github.com>
2022-04-07 22:27:36 +0300

    Add files via upload

---
+++ WpfApp1 — лаб 4/WpfApp1/Helper/FindPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.Helper
+{
+    class FindPerson
+    {
+        public int id;
+        public FindPerson(int id)
+        {
+            this.id = id;
+        }
+        public bool PersonPredicate(Person person)
+        {
+            return person.Id == id;
+        }
+    }
+}
---
+++ WpfApp1 — лаб 4/WpfApp1/Helper/FindRole.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using WpfApp1.Model;
+
+namespace WpfApp1.Helper
+{
+    public class FindRole
+    {
+       public int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+}
